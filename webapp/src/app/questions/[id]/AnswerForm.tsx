7b3a98154c2804f6b42103d2d1183619f65d76b8
Neil Cummings <trycatchlearn@outlook.com>
2025-12-03 12:07:43 +0700

    End of section 10 challenge

--- webapp/src/app/questions/[id]/AnswerForm.tsx
+++ webapp/src/app/questions/[id]/AnswerForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { postAnswer } from "@/lib/actions/question-actions";
+import {editAnswer, postAnswer} from "@/lib/actions/question-actions";
 import {answerSchema, AnswerSchema } from "@/lib/schemas/answerSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTransition } from "react";
@@ -8,6 +8,7 @@ import {Controller, useForm} from "react-hook-form";
 import {handleError} from "@/lib/util";
 import RichTextEditor from "@/components/rte/RichTextEditor";
 import {Button} from "@heroui/button";
+import {useAnswerStore} from "@/lib/hooks/useAnswerStore";
 
 type Props = {
     questionId: string;
@@ -15,21 +16,33 @@ type Props = {
 
 export default function AnswerForm({questionId}: Props) {
     const [pending, startTransition] = useTransition();
+    const editableAnswer = useAnswerStore(state => state.answer);
+    const clearAnswer = useAnswerStore(state => state.clearAnswer);
     const {control, handleSubmit, reset, formState} = useForm<AnswerSchema>({
         mode: 'onTouched',
-        resolver: zodResolver(answerSchema)
+        resolver: zodResolver(answerSchema),
+        values: {
+            content: editableAnswer?.content
+        }
     })
 
     const onSubmit = (data: AnswerSchema) => {
         startTransition(async () => {
-            const {error} = await postAnswer(data, questionId);
-            if (error) handleError(error)
-            reset();
+            if (editableAnswer) {
+                const {error} = await editAnswer(editableAnswer.id, editableAnswer.questionId, data);
+                if (error) handleError(error);
+                clearAnswer();
+                reset();
+            } else {
+                const {error} = await postAnswer(data, questionId);
+                if (error) handleError(error);
+                reset();
+            }
         })
     }
 
     return (
-        <div className='flex flex-col gap-3 items-start my-4 w-full px-6'>
+        <div id='answer-form' className='flex flex-col gap-3 items-start my-4 w-full px-6'>
             <div className='text-2xl'>Your answer</div>
             <form className='w-full flex flex-col gap-3' onSubmit={handleSubmit(onSubmit)}>
                 <Controller
@@ -50,15 +63,28 @@ export default function AnswerForm({questionId}: Props) {
                         </>
                     )}
                 />
-                <Button
-                    type='submit'
-                    isDisabled={!formState.isValid || pending}
-                    isLoading={pending || formState.isSubmitting}
-                    color='primary'
-                    className='w-fit'
-                >
-                    Post Your Answer
-                </Button>
+                <div className='flex items-start gap-3 mb-6'>
+                    <Button
+                        isDisabled={!formState.isValid || pending}
+                        isLoading={pending}
+                        color='primary'
+                        className='w-fit'
+                        type='submit'
+                    >
+                        {editableAnswer ? 'Update' : 'Post'} your answer
+                    </Button>
+                    <Button
+                        isDisabled={!editableAnswer}
+                        onPress={() => {
+                            clearAnswer();
+                            reset();
+                        }}
+                        className='w-fit'
+                        type='button'
+                    >
+                        Cancel
+                    </Button>
+                </div>
             </form>
         </div>
     );
