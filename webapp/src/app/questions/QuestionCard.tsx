2ee7229d6f68ee6ecd322bd6580339a823665d01
Neil Cummings <trycatchlearn@outlook.com>
2025-12-03 11:49:50 +0700

    End of section 10

--- webapp/src/app/questions/QuestionCard.tsx
+++ webapp/src/app/questions/QuestionCard.tsx
@@ -14,7 +14,7 @@ type Props = {
 
 export default function QuestionCard({question}: Props) {
     return (
-        <div className='flex gap-6 px-6'>
+        <div className='flex gap-6 px-6 w-full'>
             <div className='flex flex-col items-end gap-4 text-sm font-light min-w-[6rem]'>
                 <div>{question.votes} {question.votes === 1 ? 'vote' : 'votes'}</div>
                 <div
@@ -34,7 +34,7 @@ export default function QuestionCard({question}: Props) {
                 <div>{question.viewCount} {question.viewCount === 1 ? 'view' : 'views'}</div>
             </div>
             <div className='flex flex-1 justify-between min-h-32'>
-                <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-2 w-full'>
                     <Link
                         href={`/questions/${question.id}`}
                         className='text-primary font-semibold hover:underline first-letter:uppercase'
--- webapp/src/components/LinkComponent.tsx
+++ webapp/src/components/LinkComponent.tsx
@@ -4,5 +4,5 @@ import {forwardRef} from 'react';
 import Link, {LinkProps} from 'next/link';
 
 export const LinkComponent = forwardRef<HTMLAnchorElement, LinkProps>(function LinkComponent(props, ref) {
-    return <Link ref={ref} {...props} />;
+    return <Link suppressHydrationWarning ref={ref} {...props} />;
 });
